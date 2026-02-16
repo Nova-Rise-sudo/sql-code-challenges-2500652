fae3a35e57600ed9ce34ac526e332e8f6a6ed23f
Nova-Rise-sudo <anastasia.gralewski@vodafonemail.de>
2026-02-16 15:02:56 +0000

    SELECT Vorname, Nachname, Email FROM Kunden ORDER BY Nachname;

--- 01_restaurant/challenges/01_01_einladungen_ch.sql
+++ 01_restaurant/challenges/01_01_einladungen_ch.sql
@@ -2,5 +2,9 @@
 -- Diese Liste soll Vornamen, Nachnamen 
 -- und die E-Mail-Adresse der Kundinnen/Kunden enthalten.
 -- Sortieren Sie die Liste alphabetisch nach den Nachnamen.
+SELECT Vorname, Nachname, Email
+FROM Kunden
+ORDER BY Nachname;
+
 
 
--- 01_restaurant/challenges/01_02_jubilaeum_ch.sql
+++ 01_restaurant/challenges/01_02_jubilaeum_ch.sql
@@ -5,3 +5,10 @@
 -- die sie mitbringen will zugeordnet sein.
 
 -- Tipp: SQLite hat zwei verschiedene Zahlen-Datentypen INTEGER und REAL.
+CREATE TABLE Jubilaeum
+(
+KundenID INT(11,2),
+PartyGroesse Int(11,2),
+FOREIGN KEY (KundenID) REFERENCES Kunden(KundenID),
+FOREIGN KEY (PartyGroesse) REFERENCES Reservierung(PartyGroesse)
+);
